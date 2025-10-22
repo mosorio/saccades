a23f8c811ce86d9d6bda6230296385126f25e689
Maria Osorio <maria.osorio@tecnico.ulisboa.pt>
2025-10-22 20:21:24 +0100

    Add parser

--- datasets/make_all_datasets_min_max.sh
+++ datasets/make_all_datasets_min_max.sh
@@ -4,30 +4,42 @@
 # Min/max counting, just 2 different targets, no distractors
 # Test sets
 
-python3 datasets/dataset_generator.py --polar --scaling=log --luminances 0.1 0.4 0.7 --seed=0 --noise_level=0.74 --size=5000 --shapes BCDE --min_num=3 --max_num=7 --solarize --n_shapes=25 --grid=6 --n_glimpses=12 --distinctive=0.3 --not_all_equal_class_counts --fixed_background_shape=2 --task_type=min --fixed_background
-python3 datasets/dataset_generator.py --polar --scaling=log --luminances 0.3 0.6 0.9 --seed=1 --noise_level=0.74 --size=500 --shapes BCDE --min_num=3 --max_num=7 --solarize --n_shapes=25 --grid=6 --n_glimpses=12 --distinctive=0.3 --not_all_equal_class_counts --fixed_background_shape=2 --task_type=min --fixed_background
-python3 datasets/dataset_generator.py --polar --scaling=log --luminances 0.1 0.4 0.7 --seed=2 --noise_level=0.74 --size=500 --shapes CFGHJ --min_num=3 --max_num=7 --solarize --n_shapes=25 --grid=6 --n_glimpses=12 --distinctive=0.3 --not_all_equal_class_counts --fixed_background_shape=2 --task_type=min --fixed_background
-python3 datasets/dataset_generator.py --polar --scaling=log --luminances 0.3 0.6 0.9 --seed=3 --noise_level=0.74 --size=500 --shapes CFGHJ --min_num=3 --max_num=7 --solarize --n_shapes=25 --grid=6 --n_glimpses=12 --distinctive=0.3 --not_all_equal_class_counts --fixed_background_shape=2 --task_type=min --fixed_background
+#!/usr/bin/env bash
+set -euo pipefail
+
+N_G="${1:-12}"   # default 12, or pass first arg to override
+min_num="${2:-3}"  # default 3, or pass second arg to override
+max_num="${3:-7}"  # default 7, or pass third arg to override
+
+echo "min_num=$min_num"
+echo "max_num=$max_num"
+echo "n_glimpses=$N_G"
+
+
+python3 datasets/dataset_generator.py --polar --scaling=log --luminances 0.1 0.4 0.7 --seed=0 --noise_level=0.74 --size=5000 --shapes BCDE --min_num=$min_num --max_num=$max_num --solarize --n_shapes=25 --grid=6 --n_glimpses=$N_G --distinctive=0.3 --not_all_equal_class_counts --fixed_background_shape=2 --task_type=min --fixed_background
+python3 datasets/dataset_generator.py --polar --scaling=log --luminances 0.3 0.6 0.9 --seed=1 --noise_level=0.74 --size=5000 --shapes BCDE --min_num=$min_num --max_num=$max_num --solarize --n_shapes=25 --grid=6 --n_glimpses=$N_G --distinctive=0.3 --not_all_equal_class_counts --fixed_background_shape=2 --task_type=min --fixed_background
+python3 datasets/dataset_generator.py --polar --scaling=log --luminances 0.1 0.4 0.7 --seed=2 --noise_level=0.74 --size=5000 --shapes CFGHJ --min_num=$min_num --max_num=$max_num --solarize --n_shapes=25 --grid=6 --n_glimpses=$N_G --distinctive=0.3 --not_all_equal_class_counts --fixed_background_shape=2 --task_type=min --fixed_background
+python3 datasets/dataset_generator.py --polar --scaling=log --luminances 0.3 0.6 0.9 --seed=3 --noise_level=0.74 --size=5000 --shapes CFGHJ --min_num=$min_num --max_num=$max_num --solarize --n_shapes=25 --grid=6 --n_glimpses=$N_G --distinctive=0.3 --not_all_equal_class_counts --fixed_background_shape=2 --task_type=min --fixed_background
 
 # Training set
-python3 datasets/dataset_generator.py --polar --scaling=log --luminances 0.1 0.4 0.7 --seed=4 --noise_level=0.74 --size=1000 --shapes BCDE --min_num=3 --max_num=7  --solarize --n_shapes=25 --grid=6 --n_glimpses=12 --distinctive=0.3 --not_all_equal_class_counts --fixed_background_shape=2 --task_type=min --fixed_background
+python3 datasets/dataset_generator.py --polar --scaling=log --luminances 0.1 0.4 0.7 --seed=4 --noise_level=0.74 --size=100000 --shapes BCDE --min_num=$min_num --max_num=$max_num  --solarize --n_shapes=25 --grid=6 --n_glimpses=$N_G --distinctive=0.3 --not_all_equal_class_counts --fixed_background_shape=2 --task_type=min --fixed_background
 
 
 # To pretrain the ventral stream
 # Train and test sets are sampled from same distribution (no OOD generalization) which span all of the test sets above
-python3 datasets/dataset_generator.py --policy=cheat+jitter --polar --scaling=log --luminances 0.1 0.4 0.7 --seed=5 --noise_level=0.74 --size=100 --shapes BCDE --min_num=3 --max_num=7 --solarize --n_shapes=25 --grid=6 --n_glimpses=12 --distinctive=0.3 --not_all_equal_class_counts
-python3 datasets/dataset_generator.py --policy=cheat+jitter --polar --scaling=log --luminances 0.3 0.6 0.9 --seed=6 --noise_level=0.74 --size=100 --shapes BCDE --min_num=3 --max_num=7 --solarize --n_shapes=25 --grid=6 --n_glimpses=12 --distinctive=0.3 --not_all_equal_class_counts
+python3 datasets/dataset_generator.py --policy=cheat+jitter --polar --scaling=log --luminances 0.1 0.4 0.7 --seed=5 --noise_level=0.74 --size=1000 --shapes BCDE --min_num=$min_num --max_num=$max_num --solarize --n_shapes=25 --grid=6 --n_glimpses=$N_G --distinctive=0.3 --not_all_equal_class_counts
+python3 datasets/dataset_generator.py --policy=cheat+jitter --polar --scaling=log --luminances 0.3 0.6 0.9 --seed=6 --noise_level=0.74 --size=1000 --shapes BCDE --min_num=$min_num --max_num=$max_num --solarize --n_shapes=25 --grid=6 --n_glimpses=$N_G --distinctive=0.3 --not_all_equal_class_counts
 
-python3 datasets/dataset_generator.py --policy=cheat+jitter --polar --scaling=log --luminances 0.1 0.4 0.7 --seed=7 --noise_level=0.74 --size=100 --shapes CFGHJ --min_num=3 --max_num=7 --solarize --n_shapes=25 --grid=6 --n_glimpses=12 --distinctive=0.3 --not_all_equal_class_counts
-python3 datasets/dataset_generator.py --policy=cheat+jitter --polar --scaling=log --luminances 0.3 0.6 0.9 --seed=8 --noise_level=0.74 --size=100 --shapes CFGHJ --min_num=3 --max_num=7 --solarize --n_shapes=25 --grid=6 --n_glimpses=12 --distinctive=0.3 --not_all_equal_class_counts
+python3 datasets/dataset_generator.py --policy=cheat+jitter --polar --scaling=log --luminances 0.1 0.4 0.7 --seed=7 --noise_level=0.74 --size=1000 --shapes FGHJ --min_num=$min_num --max_num=$max_num --solarize --n_shapes=25 --grid=6 --n_glimpses=$N_G --distinctive=0.3 --not_all_equal_class_counts
+python3 datasets/dataset_generator.py --policy=cheat+jitter --polar --scaling=log --luminances 0.3 0.6 0.9 --seed=8 --noise_level=0.74 --size=1000 --shapes FGHJ --min_num=$min_num --max_num=$max_num --solarize --n_shapes=25 --grid=6 --n_glimpses=$N_G --distinctive=0.3 --not_all_equal_class_counts
 
-python3 datasets/dataset_generator.py --policy=cheat+jitter --polar --scaling=log --luminances 0.1 0.4 0.7 --seed=9 --noise_level=0.74 --size=1000 --shapes BCDE --min_num=3 --max_num=7 --solarize --n_shapes=25 --grid=6 --n_glimpses=12 --distinctive=0.3 --not_all_equal_class_counts
-python3 datasets/dataset_generator.py --policy=cheat+jitter --polar --scaling=log --luminances 0.3 0.6 0.9 --seed=10 --noise_level=0.74 --size=1000 --shapes BCDE --min_num=3 --max_num=7 --solarize --n_shapes=25 --grid=6 --n_glimpses=12 --distinctive=0.3 --not_all_equal_class_counts
+python3 datasets/dataset_generator.py --policy=cheat+jitter --polar --scaling=log --luminances 0.1 0.4 0.7 --seed=9 --noise_level=0.74 --size=10000 --shapes BCDE --min_num=$min_num --max_num=$max_num --solarize --n_shapes=25 --grid=6 --n_glimpses=$N_G --distinctive=0.3 --not_all_equal_class_counts
+python3 datasets/dataset_generator.py --policy=cheat+jitter --polar --scaling=log --luminances 0.3 0.6 0.9 --seed=10 --noise_level=0.74 --size=10000 --shapes BCDE --min_num=$min_num --max_num=$max_num --solarize --n_shapes=25 --grid=6 --n_glimpses=$N_G --distinctive=0.3 --not_all_equal_class_counts
 
-python3 datasets/dataset_generator.py --policy=cheat+jitter --polar --scaling=log --luminances 0.1 0.4 0.7 --seed=11 --noise_level=0.74 --size=1000 --shapes CFGHJ --min_num=3 --max_num=7 --solarize --n_shapes=25 --grid=6 --n_glimpses=12 --distinctive=0.3 --not_all_equal_class_counts
-python3 datasets/dataset_generator.py --policy=cheat+jitter --polar --scaling=log --luminances 0.3 0.6 0.9 --seed=12 --noise_level=0.74 --size=1000 --shapes CFGHJ --min_num=3 --max_num=7 --solarize --n_shapes=25 --grid=6 --n_glimpses=12 --distinctive=0.3 --not_all_equal_class_counts
+python3 datasets/dataset_generator.py --policy=cheat+jitter --polar --scaling=log --luminances 0.1 0.4 0.7 --seed=11 --noise_level=0.74 --size=10000 --shapes FGHJ --min_num=$min_num --max_num=$max_num --solarize --n_shapes=25 --grid=6 --n_glimpses=$N_G --distinctive=0.3 --not_all_equal_class_counts
+python3 datasets/dataset_generator.py --policy=cheat+jitter --polar --scaling=log --luminances 0.3 0.6 0.9 --seed=12 --noise_level=0.74 --size=10000 --shapes FGHJ --min_num=$min_num --max_num=$max_num --solarize --n_shapes=25 --grid=6 --n_glimpses=$N_G --distinctive=0.3 --not_all_equal_class_counts
 
-python3 datasets/merge_datasets_min_max.py # merge subsets
+python3 datasets/merge_datasets_min_max.py --n_glimpses $N_G --min_num $min_num --max_num $max_num # merge subsets
 
 
 # # Min/max counting, no distractors
--- datasets/merge_datasets_min_max.py
+++ datasets/merge_datasets_min_max.py
@@ -7,78 +7,42 @@ from matplotlib import pyplot as plt
 import argparse
 import os
 import re
+import sys
 
 # Set your data directory
-DATA_DIR = '/Users/mosorio/Documents/ChrisWork/saccades/datasets/image_sets_min_max/'
+DATA_DIR = '/Users/mosorio/Documents/ChrisWork/saccades/datasets/image_sets_min_max_debug/'
 
-# def merge_datasets(size_filter):
-#     """
-#     Merge all datasets in DATA_DIR matching size_filter.
-#     Automatically constructs the merged filename by keeping the
-#     original naming convention and replacing the size with the new total.
-#     """
-#     # Find matching files
-#     files = [f for f in os.listdir(DATA_DIR) if f.endswith(size_filter)]
-#     if not files:
-#         raise ValueError(f"No files found in {DATA_DIR} with filter {size_filter}")
-#     print(f"Found {len(files)} files to merge: {files}")
+p = argparse.ArgumentParser()
+p.add_argument("--n_glimpses", "-g", type=int, default=12)
+p.add_argument("--min_num", "-nmin", type=int, default=3)
+p.add_argument("--max_num", "-nmax", type=int, default=7)
 
-#     datasets, total_size = [], 0
-#     for f in files:
-#         ds = xr.open_dataset(os.path.join(DATA_DIR, f))
-#         datasets.append(ds)
-#         # Extract size (last number before .nc)
-#         match = re.search(r"_(\d+)\.nc$", f)
-#         if match:
-#             total_size += int(match.group(1))
+args = p.parse_args()
 
-#     # Concatenate
-#     merged = xr.concat(datasets, dim="image")
+# # --- Training data ---
 
-#     # Use first filename as template
-#     template = files[0]
-#     merged_name = re.sub(r"_(\d+)\.nc$", f"_{total_size}.nc", template)
-
-#     out_path = os.path.join(DATA_DIR, merged_name)
-#     merged.to_netcdf(out_path)
-
-#     print(f"Merged dataset saved to {out_path}")
-
-
-# if __name__ == "__main__":
-#     # Merge training (100000 each → 400000, etc.)
-#     merge_datasets(size_filter="_100.nc")
-
-#     # Merge test (1000 each → 4000, etc.)
-#     merge_datasets(size_filter="_10.nc")
-
-
-
-# --- Training data ---
-E01file = 'num3-7_nl-0.74_BCDEdistinct-0.3_grid6_policy-cheat+jitter_lum[0.1, 0.4, 0.7]_logpolar_12_10000.nc'
-E03file = 'num3-7_nl-0.74_BCDEdistinct-0.3_grid6_policy-cheat+jitter_lum[0.3, 0.6, 0.9]_logpolar_12_10000.nc'
-F01file = 'num3-7_nl-0.74_FGHJdistinct-0.3_grid6_policy-cheat+jitter_lum[0.1, 0.4, 0.7]_logpolar_12_10000.nc'
-F03file = 'num3-7_nl-0.74_FGHJdistinct-0.3_grid6_policy-cheat+jitter_lum[0.3, 0.6, 0.9]_logpolar_12_10000.nc'
+E01file = f'num{args.min_num}-{args.max_num}_nl-0.74_BCDEdistinct-0.3_grid6_policy-cheat+jitter_lum[0.1, 0.4, 0.7]_logpolar_{args.n_glimpses}_1000.nc'
+E03file = f'num{args.min_num}-{args.max_num}_nl-0.74_BCDEdistinct-0.3_grid6_policy-cheat+jitter_lum[0.3, 0.6, 0.9]_logpolar_{args.n_glimpses}_1000.nc'
+F01file = f'num{args.min_num}-{args.max_num}_nl-0.74_FGHJdistinct-0.3_grid6_policy-cheat+jitter_lum[0.1, 0.4, 0.7]_logpolar_{args.n_glimpses}_1000.nc'
+F03file = f'num{args.min_num}-{args.max_num}_nl-0.74_FGHJdistinct-0.3_grid6_policy-cheat+jitter_lum[0.3, 0.6, 0.9]_logpolar_{args.n_glimpses}_1000.nc'
 
 E01 = xr.open_dataset(DATA_DIR + E01file)
 E03 = xr.open_dataset(DATA_DIR + E03file)
 F01 = xr.open_dataset(DATA_DIR + F01file)
 F03 = xr.open_dataset(DATA_DIR + F03file)
 merged = xr.concat([E01, E03, F01, F03], dim='image')
-merged.to_netcdf(DATA_DIR + 'num3-7_nl-0.74_BCDEFGHJdistinct-0.3_grid6_policy-cheat+jitter_lum[0.1, 0.4, 0.7, 0.3, 0.6, 0.9]_logpolar_12_40000.nc')
+merged.to_netcdf(DATA_DIR + f'num{args.min_num}-{args.max_num}_nl-0.74_BCDEFGHJdistinct-0.3_grid6_policy-cheat+jitter_lum[0.1, 0.4, 0.7, 0.3, 0.6, 0.9]_logpolar_{args.n_glimpses}_4000.nc')
 
-
-# --- Test data ---
-E01file = 'num3-7_nl-0.74_BCDEdistinct-0.3_grid6_policy-cheat+jitter_lum[0.1, 0.4, 0.7]_logpolar_12_1000.nc'
-F01file = 'num3-7_nl-0.74_FGHJdistinct-0.3_grid6_policy-cheat+jitter_lum[0.1, 0.4, 0.7]_logpolar_12_1000.nc'
-E03file = 'num3-7_nl-0.74_BCDEdistinct-0.3_grid6_policy-cheat+jitter_lum[0.3, 0.6, 0.9]_logpolar_12_1000.nc'
-F03file = 'num3-7_nl-0.74_FGHJdistinct-0.3_grid6_policy-cheat+jitter_lum[0.3, 0.6, 0.9]_logpolar_12_1000.nc'
+# # --- Test data ---
+E01file = f'num{args.min_num}-{args.max_num}_nl-0.74_BCDEdistinct-0.3_grid6_policy-cheat+jitter_lum[0.1, 0.4, 0.7]_logpolar_{args.n_glimpses}_100.nc'
+F01file = f'num{args.min_num}-{args.max_num}_nl-0.74_FGHJdistinct-0.3_grid6_policy-cheat+jitter_lum[0.1, 0.4, 0.7]_logpolar_{args.n_glimpses}_100.nc'
+E03file = f'num{args.min_num}-{args.max_num}_nl-0.74_BCDEdistinct-0.3_grid6_policy-cheat+jitter_lum[0.3, 0.6, 0.9]_logpolar_{args.n_glimpses}_100.nc'
+F03file = f'num{args.min_num}-{args.max_num}_nl-0.74_FGHJdistinct-0.3_grid6_policy-cheat+jitter_lum[0.3, 0.6, 0.9]_logpolar_{args.n_glimpses}_100.nc'
 
 E01 = xr.open_dataset(DATA_DIR + E01file)
 E03 = xr.open_dataset(DATA_DIR + E03file)
 F01 = xr.open_dataset(DATA_DIR + F01file)
 F03 = xr.open_dataset(DATA_DIR + F03file)
 merged = xr.concat([E01, E03, F01, F03], dim='image')
-merged.to_netcdf(DATA_DIR + 'num3-7_nl-0.74_BCDEFGHJdistinct-0.3_grid6_policy-cheat+jitter_lum[0.1, 0.4, 0.7, 0.3, 0.6, 0.9]_logpolar_12_4000.nc')
-
+merged.to_netcdf(DATA_DIR + f'num{args.min_num}-{args.max_num}_nl-0.74_BCDEFGHJdistinct-0.3_grid6_policy-cheat+jitter_lum[0.1, 0.4, 0.7, 0.3, 0.6, 0.9]_logpolar_{args.n_glimpses}_400.nc')
 
