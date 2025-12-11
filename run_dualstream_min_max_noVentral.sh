020df446a958ec3ad625e5845e54f892d1bb94a3
Maria Osorio <maria.osorio@tecnico.ulisboa.pt>
2025-12-11 18:26:43 +0000

    weighting pos

--- run_dualstream_min_max_noVentral.sh
+++ run_dualstream_min_max_noVentral.sh
@@ -13,7 +13,6 @@ seed="${7:-1}"
 map_mode="${8:-both}"
 ce_weight="${9:-1.0}"
 bce_weight="${10:-1.0}"
-pos_weight="${11:-True}"
 
 
 # logdir="./logs"
@@ -21,43 +20,61 @@ pos_weight="${11:-True}"
 # ts="$(date +%Y%m%d-%H%M%S)"
 # logfile_main="${logdir}/main_onehot_min${min_num}_max${max_num}_glim${n_glimpses}_task${task}_head${head}_count_mode${count_mode}_${ts}.log"
 
-for map_mode in bce both; do    
-  logdir="./logs"
-  mkdir -p "$logdir"
-  ts="$(date +%Y%m%d-%H%M%S)"
-  logfile_main="${logdir}/main_symbolic_min${min_num}_max${max_num}_glim${n_glimpses}_task${task}_head${head}_count_mode${count_mode}_seed${seed}_map_mode${map_mode}_ce_weight${ce_weight}_bce_weight${bce_weight}_pos_weight${pos_weight}_${ts}.log"
-  echo "Running min=${min_num}, max=${max_num}, glimpses=${n_glimpses}, task=${task}, head=${head}, count_mode=${count_mode}, seed=${seed}, map_mode=${map_mode}, ce_weight=${ce_weight}, bce_weight=${bce_weight}, pos_weight=${pos_weight}"
-  echo "main log:    ${logfile_main}"
+  
+logdir="./logs"
+mkdir -p "$logdir"
+ts="$(date +%Y%m%d-%H%M%S)"
+logfile_main="${logdir}/main_symbolic_min${min_num}_max${max_num}_glim${n_glimpses}_task${task}_head${head}_count_mode${count_mode}_seed${seed}_map_mode_bce_ce_weight${ce_weight}_bce_weight${bce_weight}_pos_weight=True_${ts}.log"
+echo "Running min=${min_num}, max=${max_num}, glimpses=${n_glimpses}, task=${task}, head=${head}, count_mode=${count_mode}, seed=${seed}, map_mode_bce, ce_weight=${ce_weight}, bce_weight=${bce_weight}, pos_weight=True"
+echo "main log:    ${logfile_main}"
 
-  python3 main.py \
-    --model_type=rnn_classifier2stream --pass_penult --train_on=both \
-    --use_loss=both --opt=Adam --wd=0.00001 --distinctive=0.3 --task_type="${task}" \
-    --shape_input=symbolic --min_num="${min_num}" --max_num="${max_num}" \
-    --n_glimpses="${n_glimpses}" --h_size=1024 --train_shapes=BCDEFGHJ --test_shapes BCDEFGHJ \
-    --noise_level=0.74 --train_size=100000 --test_size=5000 --n_epochs=300 --seed=1 --map_mode="${map_mode}" \
-    --act=lrelu --dropout=0.5 --rep=0 --grid=6 --map_shape_count=8 --head="${head}" --count_mode="${count_mode}" --ce_weight="${ce_weight}" --bce_weight="${bce_weight}" --pos_weight="${pos_weight}" \
-    | tee "${logfile_main}"
+python3 main.py \
+  --model_type=rnn_classifier2stream --pass_penult --train_on=both \
+  --use_loss=both --opt=Adam --wd=0.00001 --distinctive=0.3 --task_type="${task}" \
+  --shape_input=symbolic --min_num="${min_num}" --max_num="${max_num}" \
+  --n_glimpses="${n_glimpses}" --h_size=1024 --train_shapes=BCDEFGHJ --test_shapes BCDEFGHJ \
+  --noise_level=0.74 --train_size=100000 --test_size=5000 --n_epochs=300 --seed=1 --map_mode=bce \
+  --act=lrelu --dropout=0.5 --rep=0 --grid=6 --map_shape_count=8 --head="${head}" --count_mode="${count_mode}" --ce_weight="${ce_weight}" --bce_weight="${bce_weight}" --pos_weight \
+  | tee "${logfile_main}"
+
+
+for ce_weight in 1.0 0.5; do 
+  for bce_weight in 1.0 0.5; do     
+    logdir="./logs"
+    mkdir -p "$logdir"
+    ts="$(date +%Y%m%d-%H%M%S)"
+    logfile_main="${logdir}/main_onehot_min${min_num}_max${max_num}_glim${n_glimpses}_task${task}_head${head}_count_mode${count_mode}_seed${seed}_map_mode${map_mode}_ce_weight${ce_weight}_bce_weight${bce_weight}_pos_weight=False_${ts}.log"
+    echo "Running min=${min_num}, max=${max_num}, glimpses=${n_glimpses}, task=${task}, head=${head}, count_mode=${count_mode}, seed=${seed}, map_mode=${map_mode}, ce_weight=${ce_weight}, bce_weight=${bce_weight}, pos_weight=False"
+    echo "main log:    ${logfile_main}"
+
+    python3 main.py \
+      --model_type=rnn_classifier2stream --pass_penult --train_on=both \
+      --use_loss=both --opt=Adam --wd=0.00001 --distinctive=0.3 --task_type="${task}" \
+      --shape_input=symbolic --min_num="${min_num}" --max_num="${max_num}" \
+      --n_glimpses="${n_glimpses}" --h_size=1024 --train_shapes=BCDEFGHJ --test_shapes BCDEFGHJ \
+      --noise_level=0.74 --train_size=100000 --test_size=5000 --n_epochs=300 --seed=1 --map_mode="${map_mode}" \
+      --act=lrelu --dropout=0.5 --rep=0 --grid=6 --map_shape_count=8 --head="${head}" --count_mode="${count_mode}" --ce_weight="${ce_weight}" --bce_weight="${bce_weight}" \
+      | tee "${logfile_main}"
+  done
 done
 
-for pos_weight in True False; do
-  for ce_weight in 1.0 0.5; do 
-    for bce_weight in 1.0 0.5; do     
-      logdir="./logs"
-      mkdir -p "$logdir"
-      ts="$(date +%Y%m%d-%H%M%S)"
-      logfile_main="${logdir}/main_onehot_min${min_num}_max${max_num}_glim${n_glimpses}_task${task}_head${head}_count_mode${count_mode}_seed${seed}_map_mode${map_mode}_ce_weight${ce_weight}_bce_weight${bce_weight}_pos_weight${pos_weight}_${ts}.log"
-      echo "Running min=${min_num}, max=${max_num}, glimpses=${n_glimpses}, task=${task}, head=${head}, count_mode=${count_mode}, seed=${seed}, map_mode=${map_mode}, ce_weight=${ce_weight}, bce_weight=${bce_weight}, pos_weight=${pos_weight}"
-      echo "main log:    ${logfile_main}"
+for ce_weight in 1.0 0.5; do 
+  for bce_weight in 1.0 0.5; do     
+    logdir="./logs"
+    mkdir -p "$logdir"
+    ts="$(date +%Y%m%d-%H%M%S)"
+    logfile_main="${logdir}/main_onehot_min${min_num}_max${max_num}_glim${n_glimpses}_task${task}_head${head}_count_mode${count_mode}_seed${seed}_map_mode${map_mode}_ce_weight${ce_weight}_bce_weight${bce_weight}pos_weight=True_${ts}.log"
+    echo "Running min=${min_num}, max=${max_num}, glimpses=${n_glimpses}, task=${task}, head=${head}, count_mode=${count_mode}, seed=${seed}, map_mode=${map_mode}, ce_weight=${ce_weight}, bce_weight=${bce_weight}, pos_weight=True"
+    echo "main log:    ${logfile_main}"
 
-      python3 main.py \
-        --model_type=rnn_classifier2stream --pass_penult --train_on=both \
-        --use_loss=both --opt=Adam --wd=0.00001 --distinctive=0.3 --task_type="${task}" \
-        --shape_input=symbolic --min_num="${min_num}" --max_num="${max_num}" \
-        --n_glimpses="${n_glimpses}" --h_size=1024 --train_shapes=BCDEFGHJ --test_shapes BCDEFGHJ \
-        --noise_level=0.74 --train_size=100000 --test_size=5000 --n_epochs=300 --seed=1 --map_mode="${map_mode}" \
-        --act=lrelu --dropout=0.5 --rep=0 --grid=6 --map_shape_count=8 --head="${head}" --count_mode="${count_mode}" --ce_weight="${ce_weight}" --bce_weight="${bce_weight}" --pos_weight="${pos_weight}" \
-        | tee "${logfile_main}"
-    done
+    python3 main.py \
+      --model_type=rnn_classifier2stream --pass_penult --train_on=both \
+      --use_loss=both --opt=Adam --wd=0.00001 --distinctive=0.3 --task_type="${task}" \
+      --shape_input=symbolic --min_num="${min_num}" --max_num="${max_num}" \
+      --n_glimpses="${n_glimpses}" --h_size=1024 --train_shapes=BCDEFGHJ --test_shapes BCDEFGHJ \
+      --noise_level=0.74 --train_size=100000 --test_size=5000 --n_epochs=300 --seed=1 --map_mode="${map_mode}" \
+      --act=lrelu --dropout=0.5 --rep=0 --grid=6 --map_shape_count=8 --head="${head}" --count_mode="${count_mode}" --ce_weight="${ce_weight}" --bce_weight="${bce_weight}" --pos_weight \
+      | tee "${logfile_main}"
   done
 done
 
